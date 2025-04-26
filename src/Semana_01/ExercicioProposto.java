4c8ed274380eec314df161dc5fdc072ae247699b
Adalberto Gonçalves da Silva <agsilva.mhm@gmail.com>
2025-04-26 06:26:35 -0300

    Exercício proposta para a 1a. semana(correção)

--- src/Semana_01/ExercicioProposto.java
+++ src/Semana_01/ExercicioProposto.java
@@ -1,4 +1,47 @@
 package Semana_01;
 
+import java.util.Scanner;
+
 public class ExercicioProposto {
+    public static void main(String[] args) {
+        float valor1, valor2, resultado;
+        char operador;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro número: ");
+        valor1 = input.nextFloat();
+        System.out.println("Informe o segundo número: ");
+        valor2 = input.nextFloat();
+        System.out.println("Informe a operação a ser executada (+, -, *, /): ");
+        operador = input.next().charAt(0);
+        switch (operador) {
+            case '+':
+                resultado = valor1 + valor2;
+                resultadoEscolhido("A operação escolhida foi adição ",resultado);
+            break;
+            case '-':
+                resultado = valor1 - valor2;
+                resultadoEscolhido("A operação escolhida foi subtração ",resultado);
+                break;
+            case '*':
+                resultado = valor1 * valor2;
+                resultadoEscolhido("A operação escolhida foi multiplicação ",resultado);
+                break;
+            case '/':
+                if (valor2 == 0) {
+                    System.out.println("Operação inválida!");
+                } else {
+                    resultado = valor1 / valor2;
+                    resultadoEscolhido("A operação escolhida foi divisão ", resultado);
+                }
+                break;
+            default:
+                System.out.println("Operação inválida!");
+        }
+    }
+
+    public static void resultadoEscolhido(String mensagem, float resultado){
+        System.out.println(mensagem + " o resultado foi = " + resultado);
+    }
 }
