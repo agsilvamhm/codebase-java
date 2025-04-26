a6d75bb6cb249b8064a620ccae58cdc599da1c92
Adalberto Gonçalves da Silva <agsilva.mhm@gmail.com>
2025-04-26 04:58:38 -0300

    Aula de sistema de soma

--- src/Semana_01/OlaMundo.java
+++ src/Semana_01/OlaMundo.java
@@ -1,9 +1,14 @@
 package Semana_01;
 
-import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class OlaMundo {
     public static void main(String[] args){
         System.out.println("Olá Mundo!");
+        Scanner leia = new Scanner(System.in);
+        int idade;
+        System.out.println("Qual sua idade?");
+        idade = leia.nextInt();
+        System.out.println("Minha idade é " + idade);
     }
 }
